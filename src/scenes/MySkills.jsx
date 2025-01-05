57ba8812b1794f4be58a6781096b1b6475f65f22
MCVelasquez45 <160294290+MCVelasquez45@users.noreply.github.com>
2025-01-05 13:39:38 -0700

    updating the My skill component i removed the 01,02,03

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -71,7 +71,6 @@ const MySkills = () => {
         >
           <div className="relative h-32">
             <div className="z-10">
-              <p className="font-playfair font-semibold text-5xl">01</p>
               <p className="font-playfair font-semibold text-3xl mt-3">
                 Experience
               </p>
@@ -101,7 +100,6 @@ const MySkills = () => {
         >
           <div className="relative h-32">
             <div className="z-10">
-              <p className="font-playfair font-semibold text-5xl">02</p>
               <p className="font-playfair font-semibold text-3xl mt-3">
                 Innovative
               </p>
@@ -127,7 +125,6 @@ const MySkills = () => {
         >
           <div className="relative h-32">
             <div className="z-10">
-              <p className="font-playfair font-semibold text-5xl">03</p>
               <p className="font-playfair font-semibold text-3xl mt-3">
                 Imaginative
               </p>
