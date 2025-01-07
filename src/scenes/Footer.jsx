4f9b8c9e8cf1642f2f8946b26541d30dfc23896d
MCVelasquez45 <160294290+MCVelasquez45@users.noreply.github.com>
2025-01-06 20:20:48 -0700

    updated the footer component  with link to download my resume.

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,16 +2,21 @@ import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
   return (
-    <footer
-      className="h-64 pt-10 bg-[rgb(44,188,233)]" // Correct Tailwind class for background
-    >
+    <footer className="h-64 pt-10 bg-blue-400">
       <div className="w-10/12 mx-auto">
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center mt-5">
-          <p className="font-playfair font-semibold text-2xl text-yellow">
+          <p className="font-playfair font-semibold text-2xl text-white">
             Mark Velasquez
           </p>
-          <p className="font-playfair text-md text-yellow">
+          <a
+            href="https://drive.google.com/uc?export=download&id=1RdImWCmtAC75zg0t1loFqUT8mndPVnMq"
+            download="Mark_Velasquez_Resume"
+            className="text-white underline hover:text-gray-300 transition duration-300"
+          >
+            Download Resume
+          </a>
+          <p className="font-playfair text-md text-white">
             ©2024 Velasquez. All Rights Reserved.
           </p>
         </div>
